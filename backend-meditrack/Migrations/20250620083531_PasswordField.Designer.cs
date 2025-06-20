fc0cc4981e66970429c4a78e3fc1d6705ae6add2
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-20 16:37:30 +0800

    created password migration and the controller for that

--- backend-meditrack/Migrations/20250620083531_PasswordField.Designer.cs
+++ backend-meditrack/Migrations/20250620083531_PasswordField.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend_meditrack.Data;
 
@@ -11,9 +12,11 @@ using backend_meditrack.Data;
 namespace backend_meditrack.Migrations
 {
     [DbContext(typeof(ClinicDbContext))]
-    partial class ClinicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250620083531_PasswordField")]
+    partial class PasswordField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend-meditrack/Migrations/20250620083531_PasswordField.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_meditrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Doctors");
+        }
+    }
+}
