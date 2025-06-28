358d46cfc169e8b25fb8745e599deb446f014753
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-28 15:53:01 +0800

    migrated the added schema

---
+++ backend-meditrack/Migrations/20250628075236_AddDosageToPrescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_meditrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDosageToPrescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dosage",
+                table: "Prescriptions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dosage",
+                table: "Prescriptions");
+        }
+    }
+}
